e8608fd6ea21c017d2cee4069f0c0819da6cfaff
Tarek <severos@users.noreply.github.com>
2017-10-28 08:23:31 +0200

    Added demo for binary operations

---
+++ src/Demos_Tests/Demo_Pyfhel_Binary.py
@@ -0,0 +1,48 @@
+from Pyfhel import Pyfhel
+from PyPtxt import PyPtxt
+from PyCtxt import PyCtxt
+HE = Pyfhel()
+KEYGEN_PARAMS={ "p":2,      "r":1,
+                "d":1,        "c":2,
+                "sec":80,     "w":64,
+                "L":10,       "m":-1,
+                "R":3,        "s":0,
+                "gens":[],    "ords":[]}
+
+print("Pyfhel DEMO for binary operations")
+print("   Running KeyGen with params:")
+print(KEYGEN_PARAMS)
+HE.keyGen(KEYGEN_PARAMS)
+print("  KeyGen completed")
+
+v1 = [0,0,1,1]
+v2 = [0,1,0,1]
+ones = [1]
+
+ptxt1 = PyPtxt(v1, HE)
+ptxt2 = PyPtxt(v2, HE)
+pones = PyPtxt(ones, HE)
+
+ctxt1 = HE.encrypt(ptxt1)
+ctxt2 = HE.encrypt(ptxt2)
+cones = HE.encrypt(pones, fill=1)
+
+print("Encrypted v1: ", v1)
+print("Encrypted v2: ", v2)
+#XOR operation
+ctxt1 += ctxt2      # `ctxt1 = ctxt1 + ctxt2` would also be valid
+v3 = HE.decrypt(ctxt1)
+print("v1 XOR v2 -> ", v3)
+
+print("v3: ", v3)
+print("v2: ", v2)
+#AND
+ctxt1 *= ctxt2      # `ctxt1 = ctxt1 * ctxt2` would also be valid
+v4 = HE.decrypt(ctxt1)
+print("v3 AND v2 -> ", v4)
+
+print("v4: ", v4)
+#NOT
+ctxt1 += cones
+v5 = HE.decrypt(ctxt1)
+print("NOT v4: ", v5)
