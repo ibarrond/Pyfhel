0a7207d244ffc10e0947d0afb538e1a75d75657a
AUDA <remyauda@yahoo.fr>
2017-11-13 14:59:23 +0100

    Find a bug in add and mult function. These bugs have to be corrected. Moreover, add some tests for polynolinomialMult

--- src/Demos_Tests/Demo_Pyfhel.py
+++ src/Demos_Tests/Demo_Pyfhel.py
@@ -27,15 +27,38 @@ ctxt2 = HE.encrypt(ptxt2, fill=1)
 print("Encrypted v1: ", v1)
 print("Encrypted v2: ", v2)
 
-ctxt1 += ctxt2      # `ctxt1 = ctxt1 + ctxt2` would also be valid
+ctxt1 += ctxt2  # `ctxt1 = ctxt1 + ctxt2` would also be valid->No because of a  bug in add function! Has to be corrected!
 v3 = HE.decrypt(ctxt1)
 print("add: v1 + v2 -> ", v3)
 
 print("v3: ", v3)
 print("v2: ", v2)
 
-ctxt1 *= ctxt2      # `ctxt1 = ctxt1 * ctxt2` would also be valid
+ctxt1 *= ctxt2      # `ctxt1 = ctxt1 * ctxt2` would also be valid->No because of a bug in mult function! Has to be corrected!
 v4 = HE.decrypt(ctxt1)
 print("mult: v3 * v2 -> ", v4)
 
-print(ctxt1.polynomialMult(1,2,4))
+print("------------------TEST REMY1----------------------")
+
+a0 = [3,3,3,3,3]
+a1 = [5,5,5,5,5]
+a2 = [2,2,2,2,2]
+a3 = [4,4,4,4,4]
+
+pltxt1 = PyPtxt(a0, HE)
+pltxt2 = PyPtxt(a1, HE)
+pltxt3 = PyPtxt(a2, HE)
+pltxt4 = PyPtxt(a3, HE)
+
+cytxt1 = HE.encrypt(pltxt1, fill=1)
+cytxt2 = HE.encrypt(pltxt2, fill=1)
+cytxt3 = HE.encrypt(pltxt3, fill=1)
+cytxt4 = HE.encrypt(pltxt4, fill=1)
+
+coeff = [cytxt1, cytxt2, cytxt3, cytxt4]
+
+ctxt1.polynomialMult(ctxt1, coeff)
+print("success_Polynomial")
+ctxt1polynomial = ctxt1.polynomialMult(ctxt1, coeff)
+result = HE.decrypt(ctxt1polynomial)
+print("Polynomial result: ", result)
