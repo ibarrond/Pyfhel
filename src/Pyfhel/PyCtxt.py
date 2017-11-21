2091d51ac32effbd092cb7e8a1cf381704445b3a
remyauda <remyauda@yahoo.fr>
2017-11-21 23:49:24 +0100

    [UPDATE] Update the code.

--- src/Pyfhel/PyCtxt.py
+++ src/Pyfhel/PyCtxt.py
@@ -67,14 +67,12 @@ class PyCtxt:
     # '+'operator -> Accepts both PyCtxt and Int
     def __add__(self, other):
         if not isinstance(other, (PyCtxt, int)):
-            raise TypeError("PyCtxt '+' error: lhs must be of type PyCtxt or "
-                            "int instead of " + str(type(other)))
+            raise TypeError("PyCtxt '+' error: lhs must be of type PyCtxt or int instead of " + str(type(other)))
         newCtxt = self.__pyfhel.set(self)               # Create new Ctxt for result
         if isinstance(other, PyCtxt):                   # Add directly if other is PyCtxt
             newCtxt += other
         else:
-            constCtxt = self.__pyfhel.encrypt(          # Create new PyCtxt from other if int
-                PyPtxt([other for _ in range(self.__length)],self.__pyfhel))
+            constCtxt = self.__pyfhel.encrypt(PyPtxt([other for _ in range(self.__length)],self.__pyfhel))	# Create new PyCtxt from other if int
             newCtxt += constCtxt                        # Perform addition like in '+=' operator
             del constCtxt
         return newCtxt
@@ -82,14 +80,11 @@ class PyCtxt:
     # '+=' operator
     def __iadd__(self, other):
         if not isinstance(other, (PyCtxt, int)):
-            raise TypeError("PyCtxt ADD error: lhs must be of type PyCtxt "
-                            "or int instead of type " + str(type(other)))
+            raise TypeError("PyCtxt ADD error: lhs must be of type PyCtxt or int instead of type " + str(type(other)))
         if isinstance(other, PyCtxt):
             self.__pyfhel.add(self, other, False)       # Add directly if other is PyCtxt
         else:
-            constCtxt = self.__pyfhel.encrypt(          # Create new PyCtxt from other if int
-                PyPtxt([other for _ in range(self.__length)],
-                       self.__pyfhel))
+            constCtxt = self.__pyfhel.encrypt(PyPtxt([other for _ in range(self.__length)],self.__pyfhel))	# Create new PyCtxt from other if int
             self.__pyfhel.add(self, constCtxt, False)   # Perform addition from Afhel::add
             del constCtxt
         return self
@@ -102,15 +97,12 @@ class PyCtxt:
     def __sub__(self, other):
         if not isinstance(other, PyCtxt):
             if not isinstance(other, (PyCtxt, int)):
-                raise TypeError("PyCtxt '-' error: lhs must be of type PyCtxt or "
-                            "int instead of " + str(type(other)))
+                raise TypeError("PyCtxt '-' error: lhs must be of type PyCtxt or int instead of " + str(type(other)))
         newCtxt = self.__pyfhel.set(self)
         if isinstance(other, PyCtxt):
             newCtxt -= other                            # Substract directly if other is PyCtxt
         else:
-            constCtxt = self.__pyfhel.encrypt(          # Crete new PyCtxt from other if int
-                PyPtxt([other for _ in range(self.__length)],
-                       self.__pyfhel))
+            constCtxt = self.__pyfhel.encrypt(PyPtxt([other for _ in range(self.__length)],self.__pyfhel))	# Crete new PyCtxt from other if int
             newCtxt -= constCtxt                        # Perform substraction from Afhel::add
             del constCtxt
         return newCtxt
@@ -118,8 +110,7 @@ class PyCtxt:
     # '-=' operator
     def __isub__(self, other):
         if not isinstance(other, (PyCtxt, int)):
-            raise TypeError("PyCtxt '-=' error: lhs must be of type PyCtxt "
-                            "or int instead of type " + str(type(other)))
+            raise TypeError("PyCtxt '-=' error: lhs must be of type PyCtxt or int instead of type " + str(type(other)))
         if isinstance(other, PyCtxt):
             self.__pyfhel.add(self, other, True)
         else:
@@ -136,15 +127,12 @@ class PyCtxt:
     # '*' operator
     def __mul__(self, other):
         if not isinstance(other, (PyCtxt, int)):
-            raise TypeError("PyCtxt '*' error: lhs must be of type PyCtxt or "
-                            "int instead of " + str(type(other)))
+            raise TypeError("PyCtxt '*' error: lhs must be of type PyCtxt or int instead of " + str(type(other)))
         newCtxt = self.__pyfhel.set(self)
         if isinstance(other, PyCtxt):
             newCtxt *= other
         else:
-            constCtxt = self.__pyfhel.encrypt(
-                PyPtxt([other for _ in range(self.__length)],
-                       self.__pyfhel))
+            constCtxt = self.__pyfhel.encrypt(PyPtxt([other for _ in range(self.__length)],self.__pyfhel))
             newCtxt *= constCtxt
             del constCtxt
         return newCtxt
@@ -152,14 +140,11 @@ class PyCtxt:
     # '*=' operator
     def __imul__(self, other):
         if not isinstance(other, (PyCtxt, int)):
-            raise TypeError("PyCtxt '*=' error: lhs must be of type PyCtxt "
-                            "or int instead of type " + str(type(other)))
+            raise TypeError("PyCtxt '*=' error: lhs must be of type PyCtxt or int instead of type " + str(type(other)))
         if isinstance(other, PyCtxt):
             self.__pyfhel.mult(self, other)
         else:
-            constCtxt = self.__pyfhel.encrypt(
-                PyPtxt([other for _ in range(self.__length)],
-                       self.__pyfhel))
+            constCtxt = self.__pyfhel.encrypt(PyPtxt([other for _ in range(self.__length)],self.__pyfhel))
             self.__pyfhel.mult(self, constCtxt)
             del constCtxt
         return self
@@ -170,15 +155,12 @@ class PyCtxt:
     # '%' operator - scalarProd
     def __mod__(self, other):
         if not isinstance(other, (PyCtxt, int)):
-            raise TypeError("PyCtxt '%' error: lhs must be of type PyCtxt or "
-                            "int instead of " + str(type(other)))
+            raise TypeError("PyCtxt '%' error: lhs must be of type PyCtxt or int instead of " + str(type(other)))
         newCtxt = self.__pyfhel.set(self)
         if isinstance(other, PyCtxt):
             newCtxt %= other
         else:
-            constCtxt = self.__pyfhel.encrypt(
-                PyPtxt([other for _ in range(self.__length)],
-                       self.__pyfhel))
+            constCtxt = self.__pyfhel.encrypt(PyPtxt([other for _ in range(self.__length)],self.__pyfhel))
             newCtxt %= constCtxt
             del constCtxt
         return newCtxt
@@ -186,14 +168,11 @@ class PyCtxt:
     # '%=' operator
     def __imod__(self, other):
         if not isinstance(other, (PyCtxt, int)):
-            raise TypeError("PyCtxt '%=' error: lhs must be of type PyCtxt "
-                            "or int instead of type " + str(type(other)))
+            raise TypeError("PyCtxt '%=' error: lhs must be of type PyCtxt or int instead of type " + str(type(other)))
         if isinstance(other, PyCtxt):
             self.__pyfhel.scalarProd(self, other)
         else:
-            constCtxt = self.__pyfhel.encrypt(
-                PyPtxt([other for _ in range(self.__length)],
-                       self.__pyfhel))
+            constCtxt = self.__pyfhel.encrypt(PyPtxt([other for _ in range(self.__length)],self.__pyfhel))
             self.__pyfhel.scalarProd(self, constCtxt)
             del constCtxt
         return self
@@ -204,8 +183,7 @@ class PyCtxt:
     # a ** b, b = 2|3 are the only ones supported
     def __pow__(self, other):
         if not isinstance(other, int):
-            raise TypeError("PyCtxt '**=' error: lhs must be of type  "
-                            " int instead of type " + str(type(other)))
+            raise TypeError("PyCtxt '**=' error: lhs must be of type int instead of type " + str(type(other)))
         if(other==2):
             self.__pyfhel.square(self)
         elif(other==3):
@@ -217,8 +195,7 @@ class PyCtxt:
     # a **= b, b = 2|3 are the only ones supported
     def __ipow__(self, other):
         if not isinstance(other, int):
-            raise TypeError("PyCtxt '**=' error: lhs must be of type  "
-                            " int instead of type " + str(type(other)))
+            raise TypeError("PyCtxt '**=' error: lhs must be of type int instead of type " + str(type(other)))
         if(other==2):
             self.__pyfhel.square(self)
         elif(other==3):
@@ -241,16 +218,14 @@ class PyCtxt:
     # 'lshift' operator
     def __lshift__(self, c):
         if not isinstance(c, numbers.Number):
-            raise TypeError("c '*' error: it must be of type number"
-                            "instead of " + str(type(c)))
+            raise TypeError("c '*' error: it must be of type number instead of " + str(type(c)))
         self.__pyfhel.shift(self, c)
         return self
 
     # '<<=' operator
     def __ilshift__(self, c):
         if not isinstance(c, numbers.Number):
-            raise TypeError("c '*' error: it must be of type number"
-                            "instead of " + str(type(c)))
+            raise TypeError("c '*' error: it must be of type number instead of " + str(type(c)))
         self.__pyfhel.shift(self,c)    
         return self
 
