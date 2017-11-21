d48a3b9db30cb2a4b1dac63e754880e24cc48c2d
AUDA <remyauda@yahoo.fr>
2017-11-21 23:36:30 +0100

    [UPDATE] Add a title to the graph.

--- src/Demos_Tests/Demo_Pyfhel.py
+++ src/Demos_Tests/Demo_Pyfhel.py
@@ -284,6 +284,7 @@ if args.graph:
    fig1, ax1 = plt.subplots()
    ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+   plt.title("Succesful tests versus fail tests.")
 
    plt.show()
 
