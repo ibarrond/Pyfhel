ebe4b4fa18d5979190352b4ec57d174998ec4a85
remyauda <remyauda@yahoo.fr>
2018-01-28 18:32:13 +0100

    [ADD/UPDATE] Add a .sh to install some useful packages and modify makefile.

---
+++ src/Demos_Tests/installAdditionalPackage.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# since Bash v4
+
+sudo pip install --upgrade pip
+sudo python -m pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose
+sudo apt-get install python-tk
