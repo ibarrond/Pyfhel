6c8f861f7a2be42ef59cad0aa7955ec744af42c9
ibarrond <ibarrond@eurecom.fr>
2018-07-26 15:58:13 +0000

    All encoding works and tested.

--- src/Pyfhel/test.py
+++ src/Pyfhel/test.py
@@ -22,7 +22,8 @@ class PyfhelTestCase(unittest.TestCase):
     def tearDown(self):
         sys.stderr.write('({}s) ...'.format(
             round(time.time() - self.t0 , 3)))
-             
+    def TEST_PyPtxt_PyCtxt(self):
+        pass
     def test_PyPtxt_creation_deletion(self):    
         try:
             self.ptxt = PyPtxt()
@@ -55,7 +56,9 @@ class PyfhelTestCase(unittest.TestCase):
         except Exception as err:
             self.fail("PyCtxt() deletion failed unexpectedly: ", err)
         
-    def test_Pyfhel_1_creation_deletion(self):    
+    def test_Pyfhel_1_GENERATION(self):
+        pass
+    def test_Pyfhel_1a_creation_deletion(self):    
         try:
             self.pyfhel = Pyfhel()
         except Exception as err:
@@ -65,12 +68,12 @@ class PyfhelTestCase(unittest.TestCase):
         except Exception as err:
             self.fail("Pyfhel() deletion failed unexpectedly: ", err)
             
-    def test_Pyfhel_2_context_n_key_generation(self):  
+    def test_Pyfhel_1b_context_n_key_generation(self):  
         self.pyfhel = Pyfhel()
         self.pyfhel.ContextGen(65537)
         self.pyfhel.KeyGen() 
         
-    def test_Pyfhel_3_rotate_key_generation(self):  
+    def test_Pyfhel_1c_rotate_key_generation(self):  
         self.pyfhel = Pyfhel()
         self.pyfhel.ContextGen(65537)
         self.pyfhel.KeyGen() 
@@ -80,7 +83,7 @@ class PyfhelTestCase(unittest.TestCase):
         self.assertRaises(SystemError, lambda: self.pyfhel.rotateKeyGen(61))
         self.assertRaises(SystemError, lambda: self.pyfhel.rotateKeyGen(0))
         
-    def test_Pyfhel_4_relin_key_generation(self):  
+    def test_Pyfhel_1d_relin_key_generation(self):  
         self.pyfhel = Pyfhel()
         self.pyfhel.ContextGen(65537)
         self.pyfhel.KeyGen() 
@@ -90,32 +93,94 @@ class PyfhelTestCase(unittest.TestCase):
         self.assertRaises(SystemError, lambda: self.pyfhel.relinKeyGen(61))
         self.assertRaises(SystemError, lambda: self.pyfhel.relinKeyGen(0))
         
-    def test_Pyfhel_encode_int(self):
+    def test_Pyfhel_2_ENCODING(self):
+        pass
+    
+    def test_Pyfhel_2a_encode_decode_int(self):
         self.pyfhel = Pyfhel()
         self.pyfhel.ContextGen(p=65537)
         self.pyfhel.KeyGen() 
         self.ptxt = self.pyfhel.encodeInt(127)
         self.assertEqual(self.ptxt.to_string(), b'1x^6 + 1x^5 + 1x^4 + 1x^3 + 1x^2 + 1x^1 + 1')
-        self.pyfhel.encodeInt(2, self.ptxt)
-        self.assertEqual(self.ptxt.to_string(), b'1x^1')
+        self.assertEqual(self.pyfhel.decodeInt(self.ptxt), 127)
+        self.pyfhel.encodeInt(-2, self.ptxt)
+        self.assertEqual(self.ptxt.to_string(),  b'10000x^1')
+        self.assertEqual(self.pyfhel.decodeInt(self.ptxt), -2)
                         
-    def test_Pyfhel_encode_float(self):
+    def test_Pyfhel_2b_encode_decode_float(self):
         self.pyfhel = Pyfhel()
-        self.pyfhel.ContextGen(p=10, m=8192, base=2, intDigits = 80, fracDigits = 20)
+        self.pyfhel.ContextGen(p=65537, m=8192, base=2, intDigits = 80, fracDigits = 20)
         self.pyfhel.KeyGen() 
         self.ptxt = self.pyfhel.encodeFrac(19.30)
         self.assertTrue(self.ptxt.to_string(), b'9x^8190 + 1x^4 + 1x^1 + 1')
-        self.pyfhel.encodeInt(2, self.ptxt)
-        self.assertTrue(self.ptxt.to_string(), b'1x^1')
+        self.assertEqual(round(self.pyfhel.decodeFrac(self.ptxt), 2), 19.30)
+        self.pyfhel.encodeFrac(-2.25, self.ptxt)
+        self.assertEqual(self.ptxt.to_string(),  b'1x^8190 + 10000x^1')
+        self.assertEqual(round(self.pyfhel.decodeFrac(self.ptxt), 2), -2.25)
+    
             
-    def test_Pyfhel_encode_array(self):
+    def test_Pyfhel_2c_encode_decode_batch(self):
         self.pyfhel = Pyfhel()
-        self.pyfhel.ContextGen(p=1964769281, m=8192, base=3, sec=192)
+        self.pyfhel.ContextGen(p=1964769281, m=8192, base=2, sec=192, flagBatching=True)
         self.pyfhel.KeyGen() 
         self.assertTrue(self.pyfhel.batchEnabled())
-        # self.assertEqual(self.pyfhel.getnSlots(), 8192)
-        #self.ptxt = self.pyfhel.encodeBatch([2,3])
-        #print(self.ptxt.to_string())      
+        self.ptxt = self.pyfhel.encodeBatch([1, 2, 3, 4, 5, 6])
+        self.assertEqual(self.pyfhel.getnSlots(), 8192)
+        self.assertEqual(self.pyfhel.decodeBatch(self.ptxt)[:6], [1, 2, 3, 4, 5, 6])
+        
+        #print(self.ptxt.to_string())
+        
+    def test_Pyfhel_2d_encode_decode_array(self):
+        self.pyfhel = Pyfhel()
+        self.pyfhel.ContextGen(p=1964769281, m=8192, base=2, sec=192, flagBatching=True)
+        self.pyfhel.KeyGen() 
+        self.assertTrue(self.pyfhel.batchEnabled())
+        self.ptxt = self.pyfhel.encodeArray(np.array([1, 2, 3, 4, 5, 6]))
+        self.assertEqual(self.pyfhel.getnSlots(), 8192)
+        self.assertTrue(np.alltrue(self.pyfhel.decodeArray(self.ptxt)[:6] == np.array([1, 2, 3, 4, 5, 6])))
             
+    def test_Pyfhel_3_ENCRYPTING(self):
+        pass
+    
+    def test_Pyfhel_3a_encrypt_decrypt_int(self):
+        self.pyfhel = Pyfhel()
+        self.pyfhel.ContextGen(p=65537)
+        self.pyfhel.KeyGen() 
+        self.ctxt = self.pyfhel.encryptInt(127)
+        self.assertEqual(self.pyfhel.decryptInt(self.ctxt), 127)
+        self.pyfhel.encryptInt(-2, self.ctxt)
+        self.assertEqual(self.pyfhel.decryptInt(self.ctxt), -2)
+                        
+    def test_Pyfhel_3b_encrypt_decrypt_float(self):
+        self.pyfhel = Pyfhel()
+        self.pyfhel.ContextGen(p=65537, m=8192, base=2, intDigits = 80, fracDigits = 20)
+        self.pyfhel.KeyGen() 
+        self.ptxt = self.pyfhel.encodeFrac(19.30)
+        self.assertTrue(self.ptxt.to_string(), b'9x^8190 + 1x^4 + 1x^1 + 1')
+        self.assertEqual(round(self.pyfhel.decodeFrac(self.ptxt), 2), 19.30)
+        self.pyfhel.encodeFrac(-2.25, self.ptxt)
+        self.assertEqual(self.ptxt.to_string(),  b'1x^8190 + 10000x^1')
+        self.assertEqual(round(self.pyfhel.decodeFrac(self.ptxt), 2), -2.25)
+    
+            
+    def test_Pyfhel_3c_encrypt_decrypt_batch(self):
+        self.pyfhel = Pyfhel()
+        self.pyfhel.ContextGen(p=1964769281, m=8192, base=2, sec=192, flagBatching=True)
+        self.pyfhel.KeyGen() 
+        self.assertTrue(self.pyfhel.batchEnabled())
+        self.ptxt = self.pyfhel.encodeBatch([1, 2, 3, 4, 5, 6])
+        self.assertEqual(self.pyfhel.getnSlots(), 8192)
+        self.assertEqual(self.pyfhel.decodeBatch(self.ptxt)[:6], [1, 2, 3, 4, 5, 6])
+        
+        #print(self.ptxt.to_string())
+        
+    def test_Pyfhel_3d_encrypt_decrypt_array(self):
+        self.pyfhel = Pyfhel()
+        self.pyfhel.ContextGen(p=1964769281, m=8192, base=2, sec=192, flagBatching=True)
+        self.pyfhel.KeyGen() 
+        self.assertTrue(self.pyfhel.batchEnabled())
+        self.ptxt = self.pyfhel.encodeArray(np.array([1, 2, 3, 4, 5, 6]))
+        self.assertEqual(self.pyfhel.getnSlots(), 8192)
+        self.assertTrue(np.alltrue(self.pyfhel.decodeArray(self.ptxt)[:6] == np.array([1, 2, 3, 4, 5, 6])))
 if __name__ == '__main__':
     unittest.main(verbosity=2)
