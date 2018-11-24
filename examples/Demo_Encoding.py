909d1a1fc7fd5c183cb15ff519db0e8f6a375b05
ibarrond <ibarrond@eurecom.fr>
2018-11-24 20:17:52 +0100

    Added examples for encoding and encrypting

--- examples/Demo_Encoding.py
+++ examples/Demo_Encoding.py
@@ -1,47 +1,97 @@
-# HelloWorld Demo for Pyfhel, showing the simplest use of the library by
-#   encrypting two integers, operating with them (+,-,*) and decrypting
-#   the results. The code is heavily commented.
+# Encoding Demo for Pyfhel, covering the different ways of encodeing
+#   and decodeing.
 
-
-from Pyfhel import Pyfhel, PyPtxt, PyCtxt
-# Pyfhel class contains most of the functions.
-# PyPtxt is the plaintext class
-# PyCtxt is the ciphertext class
-
+from Pyfhel import Pyfhel, PyPtxt, PyPtxt
 
 print("==============================================================")
-print("===================== Pyfhel HELLO WORLD =====================")
+print("====================== Pyfhel ENCODING =======================")
 print("==============================================================")
 
 
 print("1. Creating Context and KeyGen in a Pyfhel Object ")
-HE = Pyfhel()           # Creating empty Pyfhel object
-HE.contextGen(p=65537)  # Generating context. The value of p is important.
-                        #  There are many configurable parameters on this step
-                        #  More info in Demo_Context.py, and in the docs of
-                        #  the function (link to docs in README).
-HE.keyGen()             # Key Generation.
+HE = Pyfhel()                                       # Creating empty Pyfhel object
+HE.contextGen(p=65537, m=1024, flagBatching=True)   # Generating context. 
+# The values of p and m are chosen to enable batching (see Demo_Batching_SIMD.py)
+HE.keyGen()                                         # Key Generation.
 
+print("2. Encoding integers with encodeInt")
+integer1 = 45
+integer2 = -32
+ptxt_i1 = HE.encodeInt(integer1)   # Encoding integer1 in a new PyPtxt
+ptxt_i2 = PyPtxt()                 # Empty plaintexts have no encoding type.
+print("    Empty created ptxt_i2: ",   str(ptxt_i2))
+HE.encodeInt(integer2, ptxt_i2)    # Encoding integer2 in an existing PyPtxt
+print("    int ",integer1,'-> ptxt_i1 ', str(ptxt_i1))
+print("    int ",integer2,'-> ptxt_i2 ', str(ptxt_i2))
 
-print("2. Encrypting integers")
-integer1 = 127
-integer2 = -2
-ctxt1 = HE.encryptInt(integer1) # Encryption makes use of the public key
-ctxt2 = HE.encryptInt(integer2) # For integers, encryptInt function is used.
-print("    int ",integer1,'-> ctxt1 ', type(ctxt1))
-print("    int ",integer2,'-> ctxt2 ', type(ctxt2))
+print("3. Encoding floating point values with encodeFrac")
+float1 = 3.5
+float2 = -7.8
+ptxt_f1 = HE.encodeInt(float1)     # Encoding float1 in a new PyPtxt with encodeFrac
+ptxt_f2 = PyPtxt()
+HE.encodeFrac(float2, ptxt_f2)     # Encoding float2 in an existing PyPtxt
+print("    float ",float1,'-> ptxt_f1 ', str(ptxt_f1))
+print("    float ",float2,'-> ptxt_f2 ', str(ptxt_f2))
 
-print("3. Operating with encrypted integers")
-ctxtSum = ctxt1 + ctxt2         # `ctxt1 += ctxt2` for quicker inplace operation
-ctxtSub = ctxt1 - ctxt2         # `ctxt1 -= ctxt2` for quicker inplace operation
-ctxtMul = ctxt1 * ctxt2         # `ctxt1 *= ctxt2` for quicker inplace operation
+print("4. Encoding lists of integers using batching with encodeBatch")
+vector1 = [ 1, 2, 3, 4, 5, 6]
+vector2 = [-2, 3,-4,-3, 2,-1]
+ptxt_b1 = HE.encodeBatch(vector1)  # Encoding vector1 in a new PyPtxt with encodeBatch
+ptxt_b2 = PyPtxt()                 
+HE.encodeBatch(vector2, ptxt_f2)    # Encoding vector2 in an existing PyPtxt
+print("    list ",vector1,'-> ptxt_b1 ', str(ptxt_b1))
+print("    list ",vector2,'-> ptxt_b2 ', str(ptxt_b2))
 
+print("5. Encoding numpy 1D integer vectors using batching encodeArray")
+import numpy as np
+array1 = np.array([-6, -5, -4, -3, -2, -1])
+array2 = np.array([12, 15, 18, 21, 24, 27])
+ptxt_a1 = HE.encodeArray(array1)   # Encoding array1 in a new PyPtxt with encodeArray
+ptxt_a2 = PyPtxt()                
+HE.encodeArray(array2, ptxt_a2)    # Encoding vector2 in an existing PyPtxt
+print("    array ",array1,'-> ptxt_a1 ', str(ptxt_a1))
+print("    array ",array2,'-> ptxt_a2 ', str(ptxt_a2))
 
-print("4. Decrypting result:")
-resSum = HE.decryptInt(ctxtSum) # Decryption must use the corresponding function
-                                #  decryptInt.
-resSub = HE.decryptInt(ctxtSub) 
-resMul = HE.decryptInt(ctxtMul)
-print("     addition:       decrypt(ctxt1 + ctxt2) =  ", resSum)
-print("     substraction:   decrypt(ctxt1 - ctxt2) =  ", resSub)
-print("     multiplication: decrypt(ctxt1 + ctxt2) =  ", resMul)
+print("6. Encrypting/Decrypting all plaintexts using encryptPtxt/decryptPtxt")
+print("      HE.encrypt could also do, detecting PyPtxt as a valid type")
+print("      Just remember to operate only with ciphertexts with same encoding")
+print("     ATTENTION: HE.decrypt will directly decrypt AND decode!!!! ")
+ctxt1 = HE.encryptPtxt(ptxt_i1)
+ctxt2 = HE.encrypt(ptxt_f1)
+ctxt3 = HE.encryptPtxt(ptxt_b1)
+ctxt4 = HE.encrypt(ptxt_a1)
+
+integer1 = HE.decrypt(ctxt1)
+float1 = HE.decrypt(ctxt2)
+vector1 = HE.decrypt(ctxt3)
+
+print("7. Decoding values")
+res_i1 = HE.decodeInt(ptxt_i1) # Decoding must use the corresponding decodeing
+res_f1 = HE.decodeFrac(ptxt_f1) # Decoding must use the corresponding decodeing
+res_b1 = HE.decodeBatch(ptxt_b1) # Decoding must use the corresponding decodeing
+res_a1 = HE.decodeArray(ptxt_a1) # Decoding must use the corresponding decodeing
+
+print("    Integer (encodeInt, ENCODING_t.INTEGER, decodeInt)")
+print("      decode(ptxt_i1) =  ", res_i1)
+
+print("    Float (encodeFrac, ENCODING_t.FRACTIONAL, decodeFrac)")
+print("     decode(ptxt_f1) =  ", res_f1)
+
+print("    Batched list (encodeBatch, ENCODING_t.BATCH, decodeBatch)")
+print("     decode(ptxt_b1) =  ", res_b1)
+
+print("    NumPy 1D vector (encodeArray, ENCODING_t.BATCH, decodeArray)")
+print("     decode(ptxt_a1) =  ", res_a1)
+
+print("8. EXTRA: you can encode/decode function to guess type")
+print("    HE.encode(integer1|float1|vector1|array1)")
+print("       delegates into encodeInt|encodeFrac|encodeBatch|encodeArray depending on type")
+print("    HE.decode(PyPtxt)")
+print("       uses recorded ENCODING_t type to correctly decode")
+ptxt1 = HE.encode(integer1)
+ptxt2 = HE.encode(float1)
+ptxt3 = HE.encode(array1)
+
+integer1 = HE.decode(ptxt1)
+float1 =   HE.decode(ptxt2)
+array1 =   HE.decode(ptxt3)
--- examples/Demo_Encrypting.py
+++ examples/Demo_Encrypting.py
@@ -14,7 +14,7 @@ HE.contextGen(p=65537, m=1024, flagBatching=True)   # Generating context.
 # The values of p and m are chosen to enable batching (see Demo_Batching_SIMD.py)
 HE.keyGen()                                         # Key Generation.
 
-print("2. Encrypting integers")
+print("2. Encrypting integers with encryptInt")
 integer1 = 94
 integer2 = -235
 ctxt_i1 = HE.encryptInt(integer1)   # Encrypting integer1 in a new PyCtxt with encryptInt
@@ -24,7 +24,7 @@ HE.encryptInt(integer2, ctxt_i2)    # Encrypting integer2 in an existing PyCtxt
 print("    int ",integer1,'-> ctxt_i1 ', str(ctxt_i1))
 print("    int ",integer2,'-> ctxt_i2 ', str(ctxt_i2))
 
-print("3. Encrypting floating point values")
+print("3. Encrypting floating point values with encryptFrac")
 float1 = 3.5
 float2 = -7.8
 ctxt_f1 = HE.encryptInt(float1)     # Encrypting float1 in a new PyCtxt with encryptFrac
@@ -33,24 +33,24 @@ HE.encryptFrac(float2, ctxt_f2)     # Encrypting float2 in an existing PyCtxt
 print("    float ",float1,'-> ctxt_f1 ', str(ctxt_f1))
 print("    float ",float2,'-> ctxt_f2 ', str(ctxt_f2))
 
-print("4. Encrypting lists of integers using batching")
+print("4. Encrypting lists of integers using batching with encryptBatch")
 vector1 = [ 1, 2, 3, 4, 5, 6]
 vector2 = [-2, 3,-4,-3, 2,-1]
 ctxt_b1 = HE.encryptBatch(vector1)  # Encrypting vector1 in a new PyCtxt with encryptBatch
 ctxt_b2 = PyCtxt()                 
 HE.encryptBatch(vector2, ctxt_f2)    # Encrypting vector2 in an existing PyCtxt
-print("    vector ",vector1,'-> ctxt_b1 ', str(ctxt_b1))
-print("    vector ",vector2,'-> ctxt_b2 ', str(ctxt_b2))
+print("    list ",vector1,'-> ctxt_b1 ', str(ctxt_b1))
+print("    list ",vector2,'-> ctxt_b2 ', str(ctxt_b2))
 
-print("5. Encrypting numpy 1D integer vectors using batching")
+print("5. Encrypting numpy 1D integer vectors using batching encryptArray")
 import numpy as np
 array1 = np.array([-6, -5, -4, -3, -2, -1])
 array2 = np.array([12, 15, 18, 21, 24, 27])
 ctxt_a1 = HE.encryptArray(array1)   # Encrypting array1 in a new PyCtxt with encryptArray
 ctxt_a2 = PyCtxt()                
 HE.encryptArray(array2, ctxt_a2)    # Encrypting vector2 in an existing PyCtxt
-print("    vector ",array1,'-> ctxt_a1 ', str(ctxt_a1))
-print("    vector ",array2,'-> ctxt_a2 ', str(ctxt_a2))
+print("    array ",array1,'-> ctxt_a1 ', str(ctxt_a1))
+print("    array ",array2,'-> ctxt_a2 ', str(ctxt_a2))
 
 print("6. Operations in the encrypted domain")
 ctxt_iadd = ctxt_i1 + ctxt_i2     # `ctxt_i1 += ctxt_i2` for quicker inplace operation
@@ -73,6 +73,11 @@ ctxt_aadd = ctxt_a1 + ctxt_a2     # `ctxt_a1 += ctxt_a2` for quicker inplace ope
 ctxt_asub = ctxt_a1 - ctxt_a2     # `ctxt_a1 -= ctxt_a2` for quicker inplace operation
 ctxt_amul = ctxt_a1 * ctxt_a2     # `ctxt_a1 *= ctxt_a2` for quicker inplace operation
 
+ctxt_abadd = ctxt_a1 + ctxt_b2    # `ctxt_a1 += ctxt_b2` for quicker inplace operation
+ctxt_absub = ctxt_a1 - ctxt_b2    # `ctxt_a1 -= ctxt_b2` for quicker inplace operation
+ctxt_abmul = ctxt_a1 * ctxt_b2    # `ctxt_a1 *= ctxt_b2` for quicker inplace operation
+
+
 print("    PyCtxt Ciphertexts can only be operated with each other if:")
 print("    I.  They have the same encoding type.")
 print("        int                           - ENCODING_t.INTEGER")
@@ -86,30 +91,57 @@ res_iSum = HE.decryptInt(ctxt_iadd) # Decryption must use the corresponding decr
 res_iSub = HE.decryptInt(ctxt_isub) #  function, depending on the encoding type
 res_iMul = HE.decryptInt(ctxt_imul)
 
-res_fSum = HE.decryptInt(ctxt_fadd) # Decryption must use the corresponding decrypting
-res_fSub = HE.decryptInt(ctxt_fSub) #  function, depending on the encoding type
-res_fMul = HE.decryptInt(ctxt_fmul)
+res_fSum = HE.decryptFrac(ctxt_fadd) # Decryption must use the corresponding decrypting
+res_fSub = HE.decryptFrac(ctxt_fSub) #  function, depending on the encoding type
+res_fMul = HE.decryptFrac(ctxt_fmul)
 
-res_bSum = HE.decryptInt(ctxt_badd) # Decryption must use the corresponding decrypting
-res_bSub = HE.decryptInt(ctxt_bsub) #  function, depending on the encoding type
-res_bMul = HE.decryptInt(ctxt_bmul)
+res_bSum = HE.decryptBatch(ctxt_badd) # Decryption must use the corresponding decrypting
+res_bSub = HE.decryptBatch(ctxt_bsub) #  function, depending on the encoding type
+res_bMul = HE.decryptBatch(ctxt_bmul)
 
-res_aSum = HE.decryptInt(ctxt_aadd) # Decryption must use the corresponding decrypting
-res_aSub = HE.decryptInt(ctxt_asub) #  function, depending on the encoding type
-res_aMul = HE.decryptInt(ctxt_amul)
+res_aSum = HE.decryptArray(ctxt_aadd) # Decryption must use the corresponding decrypting
+res_aSub = HE.decryptArray(ctxt_asub) #  function, depending on the encoding type
+res_aMul = HE.decryptArray(ctxt_amul)
 
-print("     addition:       decrypt(ctxt_i1 + ctxt_i2) =  ", resSum)
-print("     substraction:   decrypt(ctxt_i1 - ctxt_i2) =  ", resSub)
-print("     multiplication: decrypt(ctxt_i1 + ctxt_i2) =  ", resMul)
+res_abSum = HE.decryptBatch(ctxt_abadd) # Decryption must use the corresponding decrypting
+res_abSub = HE.decryptBatch(ctxt_absub) #  function, depending on the encoding type
+res_abMul = HE.decryptArray(ctxt_abmul)
 
-print("     addition:       decrypt(ctxt_f1 + ctxt_f2) =  ", resSum)
-print("     substraction:   decrypt(ctxt_f1 - ctxt_f2) =  ", resSub)
-print("     multiplication: decrypt(ctxt_f1 + ctxt_f2) =  ", resMul)
+print("    Integer (encryptInt, ENCODING_t.INTEGER, decryptInt)")
+print("     addition:       decrypt(ctxt_i1 + ctxt_i2) =  ", res_iSum)
+print("     substraction:   decrypt(ctxt_i1 - ctxt_i2) =  ", res_iSub)
+print("     multiplication: decrypt(ctxt_i1 + ctxt_i2) =  ", res_iMul)
 
-print("     addition:       decrypt(ctxt_b1 + ctxt_b2) =  ", resSum)
-print("     substraction:   decrypt(ctxt_b1 - ctxt_b2) =  ", resSub)
-print("     multiplication: decrypt(ctxt_b1 + ctxt_b2) =  ", resMul)
+print("    Float (encryptFrac, ENCODING_t.FRACTIONAL, decryptFrac)")
+print("     addition:       decrypt(ctxt_f1 + ctxt_f2) =  ", res_fSum)
+print("     substraction:   decrypt(ctxt_f1 - ctxt_f2) =  ", res_fSub)
+print("     multiplication: decrypt(ctxt_f1 + ctxt_f2) =  ", res_fMul)
 
-print("     addition:       decrypt(ctxt_a1 + ctxt_a2) =  ", resSum)
-print("     substraction:   decrypt(ctxt_a1 - ctxt_a2) =  ", resSub)
-print("     multiplication: decrypt(ctxt_a1 + ctxt_a2) =  ", resMul)
+print("    Batched list (encryptBatch, ENCODING_t.BATCH, decryptBatch)")
+print("     addition:       decrypt(ctxt_b1 + ctxt_b2) =  ", res_bSum)
+print("     substraction:   decrypt(ctxt_b1 - ctxt_b2) =  ", res_bSub)
+print("     multiplication: decrypt(ctxt_b1 + ctxt_b2) =  ", res_bMul)
+
+print("    NumPy 1D vector (encryptArray, ENCODING_t.BATCH, decryptArray)")
+print("     addition:       decrypt(ctxt_a1 + ctxt_a2) =  ", res_aSum)
+print("     substraction:   decrypt(ctxt_a1 - ctxt_a2) =  ", res_aSub)
+print("     multiplication: decrypt(ctxt_a1 + ctxt_a2) =  ", res_aMul)
+
+print("    List & np 1D vec (..., ENCODING_t.BATCH, ...)")
+print("     addition:       decrypt(ctxt_a1 + ctxt_b2) =  ", res_abSum)
+print("     substraction:   decrypt(ctxt_a1 - ctxt_b2) =  ", res_abSub)
+print("     multiplication: decrypt(ctxt_a1 + ctxt_b2) =  ", res_abMul)
+
+
+print("8. EXTRA: you can encrypt/decrypt function to guess type")
+print("    HE.encrypt(integer1|float1|vector1|array1)")
+print("       delegates into encryptInt|encryptFrac|EncryptBatch|envryptArray depending on type")
+print("    HE.decrypt(PyCtxt)")
+print("       uses recorded ENCODING_t type to correctly decrypt")
+ctxt1 = HE.encrypt(integer1)
+ctxt2 = HE.encrypt(float1)
+ctxt3 = HE.encrypt(array1)
+
+integer1 = HE.decrypt(ctxt1)
+float1 =   HE.decrypt(ctxt2)
+array1 =   HE.decrypt(ctxt3)
